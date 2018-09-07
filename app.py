52562c27e79ee617ae486b5f1002f4cb8744a291
hanb <936767517@qq.com>
2018-09-07 09:35:25 +0800

    add kongge

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def index():
 class TaskListAPI(Resource):
     def get(self):
         return "hello world"
+
 api.add_resource(TaskListAPI,'/hosts',endpoint='host')
 if __name__ == '__main__':
     app.run(debug=True)
